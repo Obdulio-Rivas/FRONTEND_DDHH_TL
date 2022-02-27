3eca8adb1b9e9a0c5a7f87e95b6d5ace0186f8cf
Obdulio-Rivas <obdulior350@gmail.com>
2022-02-27 16:50:30 -0600

    Agregando las rutas

--- src/pages/Incidents/NewIncident/NewIncident.jsx
+++ src/pages/Incidents/NewIncident/NewIncident.jsx
@@ -5,6 +5,10 @@ import { useLocation } from "react-router-dom";
 import Step1 from "./Steps/Step1";
 import Step2 from "./Steps/Step2";
 import Step3 from "./Steps/Step3";
+import Step4 from "./Steps/Step4";
+import Step5 from "./Steps/Step5";
+import Step6 from "./Steps/Step6";
+import Step7 from "./Steps/Step7";
 import StepManager from "./StepManager";
 
 const NewIncident = () => {
@@ -12,24 +16,32 @@ const NewIncident = () => {
   const [store, setStore] = useState({});
 
   const handlerStore = (values) => {
-    setStore((prevStore)=> {
+    setStore((prevStore) => {
       return {
         ...prevStore,
-        ...values
-      }
-    })
-  }
+        ...values,
+      };
+    });
+  };
 
-  const getStepForm = (pathname = '') => {
-    const pathStep = pathname.split('/');
-    const indexPathStep = pathname.split('/').length - 1;
+  const getStepForm = (pathname = "") => {
+    const pathStep = pathname.split("/");
+    const indexPathStep = pathname.split("/").length - 1;
     switch (pathStep[indexPathStep]) {
-      case 'step1':
-        return <Step1 handlerStore={handlerStore}/>;
-      case 'step2':
+      case "step1":
+        return <Step1 handlerStore={handlerStore} />;
+      case "step2":
         return <Step2 handlerStore={handlerStore} />;
-      case 'step3':
-        return <Step3 store={store}/>;
+      case "step3":
+        return <Step3 handlerStore={handlerStore} />;
+      case "step4":
+        return <Step4 handlerStore={handlerStore} />;
+      case "step5":
+        return <Step5 handlerStore={handlerStore} />;
+      case "step6":
+        return <Step6 handlerStore={handlerStore} />;
+      case "step7":
+        return <Step7 store={store} />;
 
       default:
         return <Step1 />;
--- src/routes/Incident/IncidentRoutes.js
+++ src/routes/Incident/IncidentRoutes.js
@@ -10,6 +10,10 @@ const IncidentRoutes = () => {
       <Route path="/step1" element={<NewIncident />} />
       <Route path="/step2" element={<NewIncident />} />
       <Route path="/step3" element={<NewIncident />} />
+      <Route path="/step4" element={<NewIncident />} />
+      <Route path="/step5" element={<NewIncident />} />
+      <Route path="/step6" element={<NewIncident />} />
+      <Route path="/step7" element={<NewIncident />} />
       <Route path="*" element={<span>Pagina 404 - Content not found!</span>} />
     </Routes>
   );
