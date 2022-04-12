2a8930646e0f59aef13832b795c7454bf957f176
Yohalmo <yoernest.96@gmail.com>
2022-04-11 18:56:59 -0600

    Insertando el incidente del cliente.

--- src/pages/Incidents/NewIncident/Steps/Step8.jsx
+++ src/pages/Incidents/NewIncident/Steps/Step8.jsx
@@ -7,52 +7,15 @@ import { useNavigate } from "react-router-dom";
 import AuthService from "../../../../services/Auth/Auth.Service";
 import VictimService from "../../../../services/Victim/Victim.Service";
 import IncidentVictimsService from "../../../../services/IncidentVictims/IncidentVictims.Service";
+import CaseService from "../../../../services/Incident/Incident.Service";
 
 const Step8 = ({ store, handlerStore }) => {
-    const step1 = store.step1.values;
-    const step3 = store.step3.values;
-    const step4 = store.step4.values;
-    const step5 = store.step5.values;
-    const step6 = store.step6.values;
+    const step1 = store.step1?.values;
+    const step3 = store.step3?.values;
+    const step4 = store.step4?.values;
+    const step5 = store.step5?.values;
+    const step6 = store.step6?.values;
   const [currentUser, setCurrentUser] = useState(AuthService.getCurrentUser());
-  const [currentIncident, setCurrentIncident] = useState({
-    //registro institucional
-    expediente: "",
-    incident_date: "",
-    hour: "",
-    incident_institution: 0,
-    incident_institution_name: "",
-    name_reference: "",
-    contact: "",
-    incident_identification: "",
-    //datos especificos del caso
-    date_hechos: "",
-    incident_time: "",
-    adress: "",
-    deparment: 0,
-    municipality: 0,
-    cause_displacement: "",
-    people_displacement: "",
-    institutions_accompanied: "",
-    statal_institution: 0,
-    statal_institution_name: "",
-    accompanied_descriptions: "",
-    //perfil socioeconomico
-    home: 0,
-    monthly_income: 0.00,
-    familiar_income: 0.00,
-    survive_displacement: "",
-    //perfil migratorio
-    country_leave: 0,
-    country_leave_name: "",
-    family_cant: "",
-    //general
-    status: 0,
-    description_incident: "",
-    //Foreign key.
-    id_type_incident: 0,
-    id_user: 0,
-  });
   const { register, handleSubmit } = useForm();
   const navigate = useNavigate();
 
@@ -77,6 +40,62 @@ const Step8 = ({ store, handlerStore }) => {
     let id_user = currentUser.id_user;
     let id_incident = 0;
     let array = [];
+    let cause_displacement ="";
+    let people_displacement ="";
+    let institutions_accompanied="";
+    let survive_displacement="";
+
+    for (let i = 0; i < step3.cause_displacement.length; i++) {
+      cause_displacement = cause_displacement+' - '+step3.cause_displacement[i]; 
+    }
+    for (let i = 0; i < step3.people_displacement.length; i++) {
+      people_displacement = people_displacement+' - '+step3.people_displacement[i];
+    }
+    for (let i = 0; i < step3.institutions_accompanied.length; i++) {
+      institutions_accompanied = institutions_accompanied+' - '+step3.institutions_accompanied[i];
+    }
+    for (let i = 0; i < step4.survive_displacement.length; i++) {
+      survive_displacement = survive_displacement+' - '+step4.survive_displacement[i];
+    }
+
+    let Incidentobject = {
+        expediente : step1.expediente,
+        incident_date: step1.incident_date,
+        hour: step1.incident_date,
+        incident_institution: step1.incident_institution,
+        incident_institution_name: step1.incident_institution_name,
+        name_reference: step1.name_reference,
+        contact: step1.contact,
+        incident_identification: step1.incident_identification,
+        //datos especificos del caso
+        date_hechos: step3.date_hechos,
+        datetime_hechos : step3.datetime_hechos,
+        incident_time: "",
+        adress: step3.adress,
+        deparment: step3.deparment,
+        municipality: step3.municipality,
+        cause_displacement: cause_displacement,
+        people_displacement: people_displacement,
+        institutions_accompanied: institutions_accompanied,
+        statal_institution: step3.statal_institution,
+        statal_institution_name: step3.statal_institution_name,
+        accompanied_descriptions: step3.accompanied_descriptions,
+        //perfil socioeconomico
+        home: step4.home,
+        monthly_income: step4.monthly_income,
+        familiar_income: step4.familiar_income,
+        survive_displacement: survive_displacement,
+        //perfil migratorio
+        country_leave: step5.country_leave,
+        country_leave_name: step5.country_leave_name,
+        family_cant: step5.family_cant,
+        //general
+        status: 0,
+        description_incident: step6.description_incident,
+        //Foreign key.
+        id_type_incident: 0,
+        id_user: id_user,
+    };
 
     handlerStore({
       step8: {
@@ -85,7 +104,7 @@ const Step8 = ({ store, handlerStore }) => {
       },
     });
 
-    setCurrentIncident(
+    /*setCurrentIncident(
       {expediente : step1.expediente,
         incident_date: step1.incident_date,
         hour: step1.incident_date,
@@ -122,22 +141,20 @@ const Step8 = ({ store, handlerStore }) => {
         id_type_incident: 0,
         id_user: 0,
       }
-    )
-    console.log(step1)
-    console.log(step3)
-    console.log(step4)
-    console.log(step5)
-    console.log(step6)
-    console.log(currentIncident);
+    )*/
+    //console.log(Incidentobject);
+
     /** Registrar Incidente */
-    
+      let caseResponse = await CaseService.postIncident(Incidentobject);
+      id_incident=caseResponse.data.id_incident;
+      console.log(caseResponse);
 
-
+    /**Registrar las victimas*/
     for (let i = 0; i < store.step7.values.length; i++) {
       let response = await VictimService.postVictim(store.step7.values[i]);
       array.push(response.data.id_victim)
     }
-    
+    /**Registrar la relacion de incidente victima */
     array.map(async (_id_victim) => {
       let newIncidentVictim = {
         id_incident: id_incident,
